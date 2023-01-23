79c9dd42c571c6a14f4928be637f584df67c68ef
Sven Schaper <sven.schaper@limeanchor.com>
2023-01-23 22:12:35 +0100

    Update stripe.go

--- stripe.go
+++ stripe.go
@@ -95,7 +95,7 @@ func (web *WebAdmin) CreateCheckoutSessionBasic(w http.ResponseWriter, r *http.R
 }
 
 func (web *WebAdmin) CreatePortalSession(w http.ResponseWriter, r *http.Request) {
-	returnurl := "https://" + web.Domain + "/admin"
+	returnurl := "https://" + web.Domain + "/"
 	customerId := r.Header.Get("customer")
 
 	// Authenticate your user.
